aff5578e6ebd952e82783c0c2851624f2bb7a17a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-18 12:31:43 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ async function getData() {
     const result = await fetch('https://randomuser.me/api?results=12');
     const characters = await result.json();
     console.log(characters);
+    characters.results.forEach(element => {
+     const box = document.createRange().createContextualFragment(`
+        
+        <div class="box box-1">
+            <div class="test-row-img">
+                <img src="${element.picture.large}" alt="">
+                    <div class="test-row-name">
+                        <h5>Anna Wallace</h5>
+                        <h5 class="buyer">Verified Buyer</h5>
+                    </div>
+            </div>
+            <div class="test-row-text">
+                <p>“ Lorem ipsum dolor sit amet consectetur. Convallis nam aliquet odio amet morbi amet. Adipiscing facilisi cras tempus eget sodales. Ac at dictum malesuada leo massa non. Et non vitae placerat nunc nisi ”</p>
+            </div>
+        </div>
+        
+        `)
+        const text_row  = document.querySelector('.test-row');
+        text_row.append(box);
+    });
 }
 
 getData()
